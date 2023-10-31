2075eb9e8366cc8ac62492f76163392cc6a56729
laurafry14 <laura.fry@mun.ca>
2023-10-31 13:48:41 -0230

    edits made

--- src/main/java/com/keyin/ClientApplication.java
+++ src/main/java/com/keyin/ClientApplication.java
@@ -11,8 +11,25 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ClientApplication {
-    private static final RESTClient restClient = new RESTClient();
+//    private static final RESTClient restClient = new RESTClient();
+    private RESTClient restClient;
+
+    public RESTClient getRestClient() {
+        if (restClient == null) {
+            restClient = new RESTClient();
+        }
+        return restClient;
+    }
+
+    public void setRestClient(RESTClient restClient) {
+        this.restClient = restClient;
+    }
+
     public static void main(String[] args) {
+        ClientApplication cliApp = new ClientApplication();
+
+        cliApp.setRestClient(new RESTClient());
+
         Scanner scanner = new Scanner(System.in);
         printMenuAndUsage();
 
@@ -33,16 +50,16 @@ public class ClientApplication {
                 String subOption = inputParts[2];
                 switch (subOption) {
                     case "aircraft":
-                        getAllAircraft();
+                        cliApp.generateAircraftReport();
                         break;
                     case "airports":
-                        // getAllAirports();
+                        cliApp.generateAirportReport();
                         break;
                     case "passengers":
-                        getAllPassenger();
+                        cliApp.generatePassengerReport();
                         break;
                     case "cities":
-                        // getAllCities();
+                        cliApp.generateCityReport();
                         break;
                     default:
                         System.out.println("Invalid sub-option. Please choose a valid sub-option.");
@@ -95,9 +112,85 @@ public class ClientApplication {
         System.out.println(" --show airportpassenger <id>         shows which airports are used by a specific passenger");
     }
 
+    public String generateAircraftReport() {
+        List<Aircraft> aircrafts = getRestClient().getAllAircraft();
 
+        StringBuffer report = new StringBuffer();
 
+        for (Aircraft  aircraft : aircrafts) {
+            report.append(aircraft.getType());
+            report.append(" - ");
+            report.append(aircraft.getAirlineName());
 
+            if (aircrafts.indexOf(aircraft) != (aircrafts.size() - 1)) {
+                report.append(",");
+            }
+        }
+
+        System.out.println(report.toString());
+
+        return report.toString();
+    }
+
+    public String generateAirportReport() {
+        List<Airport> airports = getRestClient().getAllAirports();
+
+        StringBuffer report = new StringBuffer();
+
+        for (Airport airport : airports) {
+            report.append(airport.getName());
+            report.append(" - ");
+            report.append(airport.getCode());
+
+            if (airports.indexOf(airport) != (airports.size() - 1)) {
+                report.append(",");
+            }
+        }
+
+        System.out.println(report.toString());
+
+        return report.toString();
+    }
+
+    public String generateCityReport() {
+        List<City> cities = getRestClient().getAllCity();
+
+        StringBuffer report = new StringBuffer();
+
+        for (City city : cities) {
+            report.append(city.getName());
+            report.append(" - ");
+            report.append(city.getState());
+
+            if (cities.indexOf(city) != (cities.size() - 1)) {
+                report.append(",");
+            }
+        }
+
+        System.out.println(report.toString());
+
+        return report.toString();
+    }
+
+    public String generatePassengerReport() {
+        List<Passenger> passengers = getRestClient().getAllPassengers();
+
+        StringBuffer report = new StringBuffer();
+
+        for (Passenger passenger : passengers) {
+            report.append(passenger.getFirstName() + " " + passenger.getLastName());
+            report.append(" - ");
+            report.append(passenger.getPhoneNumber());
+
+            if (passengers.indexOf(passenger) != (passengers.size() - 1)) {
+                report.append(",");
+            }
+        }
+
+        System.out.println(report.toString());
+
+        return report.toString();
+    }
 
 //    public static void getAirportsByCity() {
 //        System.out.println("Enter the desired city:");
@@ -134,127 +227,36 @@ public class ClientApplication {
 //        List airportsJson = restClient.getAllAirportsAsJson();
 //        System.out.println("Airports used by passenger " + passenger + ": " + airportsJson);
 //    }
-
-
-    public static void getAllPassenger() {
-        List<Passenger> passengerList;
-        try {
-            passengerList = restClient.getAllPassenger();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-        int passengerCount = 0;
-        for (Passenger passenger : passengerList) {
-            System.out.printf("\nPassenger(%s):\n********\n", passengerCount);
-            System.out.println(passenger.passengerToString());
-            passengerCount++;
-        }
-    }
-
-    public static void getAllAircraft() {
-        List<Aircraft> airCraftList;
-        try {
-            airCraftList = restClient.getAllAircraft();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-        int aircraftCount = 0;
-        for (Aircraft aircraft : airCraftList) {
-            System.out.printf("\nAircraft(%s):\n********\n", aircraftCount);
-            System.out.println("\tID:                 " + aircraft.getId());
-            System.out.println("\tType:               " + aircraft.getType());
-            System.out.println("\tAirline Name:       " + aircraft.getAirlineName());
-            System.out.println("\tPassenger Capacity: " + aircraft.getNumberOfPassengers());
-            System.out.println("\tAirport ID List:    ");
-            int airportCount = 0;
-            for (long id : aircraft.getAirportIdList()) {
-                System.out.printf("\t\tAirport (%s)\n", airportCount);
-                getAirportById(id);
-                airportCount++;
-            }
-            System.out.println("\tPassenger ID List:  " + aircraft.getPassengerIdList());
-            aircraftCount++;
-        }
-    }
-
-    public static void getAllCity() {
-        List<City> cityList;
-        try {
-            cityList = restClient.getAllCity();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-        int cityCount = 0;
-        for (City city : cityList) {
-            System.out.printf("\nCity(%s):\n********\n", cityCount);
-            System.out.println("\tID:                 " + city.getId());
-            System.out.println("\tCity Name:          " + city.getName());
-            System.out.println("\tState:              " + city.getState());
-            System.out.println("\tPopulation:         " + city.getPopulation());
-            System.out.println("\tAirport ID List:       ");
-            int airportCount = 0;
-            for (long id : city.getAirportIdList()) {
-                System.out.printf("\t\tAirport (%s)\n", airportCount);
-                getAirportById(id);
-                airportCount++;
-            }
-            cityCount ++;
-        }
-    }
-
-    public static void getCityById(long id) {
-        City city;
-        try {
-            city = restClient.getCityById(id);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-        System.out.println("\t\t\t" + city);
-    }
-
-    public static void getAllAirport() {
-        List<Airport> airportList;
-        try {
-            airportList = restClient.getAllAirport();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-        int airportCount = 0;
-        for (Airport airport : airportList) {
-            System.out.printf("\nAirport(%s):\n********\n", airportCount);
-            System.out.println("\tID:                 " + airport.getId());
-            System.out.println("\tName:               " + airport.getName());
-            System.out.println("\tCode:       " + airport.getCode());
-            System.out.println("\tAirport ID List:    ");
-            getCityById(airport.getCityId());
-            airportCount++;
-        }
-    }
-
-    public static void getPassengerById(long id) {
-        Passenger passenger;
-        try {
-            passenger = restClient.getPassengerById(id);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-        System.out.println("\t\t\t" + passenger);
-    }
-
-    public static void getAirportById(long id) {
-        Airport airport;
-        try {
-            airport = restClient.getAirportById(id);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-        System.out.println("\t\t\t" + airport);
-    }
+//    public static void getCityById(long id) {
+//        City city;
+//        try {
+//            city = restClient.getCityById(id);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return;
+//        }
+//        System.out.println("\t\t\t" + city);
+//    }
+//
+//    public static void getPassengerById(long id) {
+//        Passenger passenger;
+//        try {
+//            passenger = restClient.getPassengerById(id);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return;
+//        }
+//        System.out.println("\t\t\t" + passenger);
+//    }
+//
+//    public static void getAirportById(long id) {
+//        Airport airport;
+//        try {
+//            airport = restClient.getAirportById(id);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return;
+//        }
+//        System.out.println("\t\t\t" + airport);
+//    }
 }
